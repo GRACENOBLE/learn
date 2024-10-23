4cf3c5c24e9d0132fb8ae37c1d4e5890e1717fd8
GRACENOBLE <gracenoble72@gmail.com>
2024-10-23 20:07:59 +0300

    go client

---
+++ cryptomasters/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"frontendmasters.com/go/crypto/api"
+)
+
+func main(){
+	rate, err := api.GetRate("BTC")
+	if err == nil {
+		fmt.Printf("The rate for %v is %.2f", rate.Currency, rate.Price)
+	}
+}
