65f6de7d801080f7abd72ec4ba64a1e1d7874709
GRACENOBLE <gracenoble72@gmail.com>
2024-10-24 07:18:53 +0300

    testing go

--- cryptomasters/main.go
+++ cryptomasters/main.go
@@ -2,13 +2,29 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"frontendmasters.com/go/crypto/api"
 )
 
-func main(){
-	rate, err := api.GetRate("BTC")
+func main() {
+	currencies := []string{"BTC", "ETH", "BCH"}
+
+	var wg sync.WaitGroup
+
+	for _, curreny := range currencies {
+		wg.Add(1)
+		go func() {
+			getCurrencyData(curreny)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+func getCurrencyData(currency string) {
+	rate, err := api.GetRate(currency)
 	if err == nil {
-		fmt.Printf("The rate for %v is %.2f", rate.Currency, rate.Price)
+		fmt.Printf("The rate for %v is %.2f\n", rate.Currency, rate.Price)
 	}
 }
